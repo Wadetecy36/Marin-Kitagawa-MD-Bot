2627360c305b79ae2d6561f45aeef6248a2aef91
Wadetecy36 <mensahdenzel285@gmail.com>
2024-12-07 00:09:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,18 +7,18 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918373017400']
-global.OwnerNumber = ['918373017400']
-global.ownertag = ['918373017400']
-global.BotName = "𝑀𝒶𝓇𝒾𝓃-ᴋɪᴛᴀɢᴀᴡᴀ"
+global.Owner = ['233570578965']
+global.OwnerNumber = ['233570578965']
+global.ownertag = ['233570578965']
+global.BotName = "Pixel"
 global.packname = "𝑀𝒶𝓇𝒾𝓃-ʙᴏᴛ"
-global.author = "By: ᥴꫝꫀꪗ-𝙎𝙖𝙣💫🌙✨"
-global.OwnerName = "ᥴꫝꫀꪗ-𝙎𝙖𝙣💫🌙✨"
+global.author = "By: wade-𝙎𝙖𝙣💫🌙✨"
+global.OwnerName = "wade-𝙎𝙖𝙣💫🌙✨"
 global.BotSourceCode = "https://github.com/Chey-san/Marin-Kitagawa-MD-Bot"
 global.SupportGroupLink = "https://chat.whatsapp.com/G0dZXqJAL9b1boqnra7eb4"
 global.sessionName = "session"
 global.prefa = ['-']
-global.location = "West Bengal, India"
+global.location = "Accra, Ghana"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://telegra.ph/file/17ecc2eece63a4d8bcd51.mp4' }
